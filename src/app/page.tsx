6b969ad8ff662579f96e2b7f6f5b5c9b801ee104
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2025-01-27 00:47:51 +1300

    ui updates & hold down button to talk to assistant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@
 import { useMicVAD, utils } from '@ricky0123/vad-react'
 // import { useState } from 'react'
 import { Howl } from 'howler'
-import { Switch } from '@/components/ui/switch'
 import { useRef } from 'react'
+import clsx from 'clsx'
+import { Mic } from 'lucide-react'
 
 export const Demo = () => {
   const currentMessages = useRef([{}])
@@ -15,6 +16,7 @@ export const Demo = () => {
       const blob = new Blob([wavBuffer], { type: 'audio/wav' })
 
       const formData = new FormData()
+      console.log('message sent from client side', currentMessages.current)
       formData.append('file', blob, 'audio.wav')
       formData.append('messages', JSON.stringify(currentMessages.current))
       let data
@@ -31,8 +33,10 @@ export const Demo = () => {
         data = await res.json()
         const messages = data.messages
 
+        console.log('messages recieved on client side', messages)
+
         // Update current messages with the new response
-        currentMessages.current = [...currentMessages.current, ...messages]
+        currentMessages.current = [...JSON.parse(messages)]
       } catch (error) {
         console.error('Error sending data to OpenAI API:', error)
         return
@@ -47,14 +51,32 @@ export const Demo = () => {
   })
 
   return (
-    <div>
-      <Switch
-        onCheckedChange={() => {
-          vad.toggle()
-        }}
-        disabled={vad.loading}
-      />
-      <button onClick={() => console.log(currentMessages.current)}>log ref current</button>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className=" relative">
+        <button
+          onMouseDown={() => {
+            vad.start()
+          }}
+          onMouseUp={() => {
+            setTimeout(() => {
+              vad.pause()
+            }, 2000)
+          }}
+          className="rounded-full border border-red-500 p-5"
+        >
+          <Mic className="text-2xl" />
+        </button>
+        <div
+          className={clsx(
+            'absolute size-36 blur-3xl rounded-full bg-gradient-to-b from-red-200 to-red-400 dark:from-red-600 dark:to-red-800 -z-50 transition ease-in-out top-0 left-0 -translate-x-[50px] -translate-y-[50px]',
+            {
+              'opacity-0': vad.loading || vad.errored,
+              'opacity-30': !vad.loading && !vad.errored && !vad.userSpeaking,
+              'opacity-100 scale-110 pulse': vad.userSpeaking,
+            },
+          )}
+        />
+      </div>
     </div>
   )
 }
